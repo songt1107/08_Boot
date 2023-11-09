8b5529ff55d5602d962febb633a4e7a605771b60
songt1107 <songt1107@gmail.com>
2023-11-09 14:56:55 +0900

    wc2

---
+++ boardProject-boot/src/main/java/edu/kh/project/common/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package edu.kh.project.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		
+		return new BCryptPasswordEncoder();
+		
+	}
+	
+}
